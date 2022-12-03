b09e7b26ed3863964d136f56e55e003e36507472
KotenAngered <113610915+KotenAngered@users.noreply.github.com>
2022-12-03 11:00:09 +0100

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,20 @@
+import discord
+import praw
+
+client = discord.Client()
+reddit = praw.Reddit(
+    client_id='client id here',
+    client_secret='client secret here',
+    user_agent='user agent here,
+    username='username here'
+    password='password here'
+)
+
+@client.event
+async def on_ready():
+    # Get posts from the "popular" subreddit and post them to the "general" channel
+    for post in reddit.subreddit('popular').top('week'):
+        channel = discord.utils.get(client.get_all_channels(), name='general')
+        await channel.send(post.title + ': ' + post.url)
+
+client.run('bot token here")
